48182676b05a8cc8551a44cf8911f7bc9d232c59
DoctorRon64 <piepmail@lefthandmedia.com>
2023-09-18 17:38:00 +0200

    Properties and animations

--- TamagotchiMauiApp/TamagotchiMauiApp/MainPage.xaml.cs
+++ TamagotchiMauiApp/TamagotchiMauiApp/MainPage.xaml.cs
@@ -1,9 +1,26 @@
-﻿namespace TamagotchiMauiApp;
+﻿using Microsoft.Maui.Controls;
+using System.ComponentModel;
 
-public partial class MainPage : ContentPage
+namespace TamagotchiMauiApp;
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
+	public event PropertyChangedEventHandler PropertyChanged;
+	private float Hunger { get; set; } = 100.0f;
+	public string HungerText => Hunger switch
+	{
+		0 => "Mijn buik is lekker rond! ",
+		< 25 => "O een kleine lekkere late night snack kan er wel in. ",
+		< 50 => "Ah Ooooh De lunch begint eraan te komen. ",
+		< 75 => "IK ZO'N ERGE HONGER!!!!! ",
+		< 85 => "OMG JE ZIET MIJN BOTTEN BIJNA ZO VEEL HONGER HEB IK!!!",
+		< 90 => "Ik sterf!",
+		< 100 => "Dag mijn Jongen",
+	};
+
 	public MainPage()
 	{
+		BindingContext = this;
+
 		InitializeComponent();
 	}
 
@@ -11,6 +28,11 @@ public partial class MainPage : ContentPage
 	{
 		SemanticScreenReader.Default.Announce("To Food Hall");
 		Navigation.PushAsync(new FoodPage());
+	}
+
+	private void GeefEten(object sender, EventArgs e)
+	{
+		Hunger -= 1.0f;
 	}
 
 	private void ToSlurpHallFunction(object sender, EventArgs e)
--- TamagotchiMauiApp/TamagotchiMauiApp/MauiProgram.cs
+++ TamagotchiMauiApp/TamagotchiMauiApp/MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("VanillaRavioli.ttf", "VanillaRavioli");
+				fonts.AddFont("DeputyJack.ttf", "DeputyJack");
+				fonts.AddFont("MonkeyBanana.ttf", "MonkeyBanana");
 			});
 
 #if DEBUG
