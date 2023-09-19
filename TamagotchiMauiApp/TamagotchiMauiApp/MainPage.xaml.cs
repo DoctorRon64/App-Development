bed5a14356716a7d315f2c3c1093efde313a099d
DoctorRon64 <piepmail@lefthandmedia.com>
2023-09-19 11:58:56 +0200

    Update propertie bindings but does not work

--- TamagotchiMauiApp/TamagotchiMauiApp/MainPage.xaml.cs
+++ TamagotchiMauiApp/TamagotchiMauiApp/MainPage.xaml.cs
@@ -5,17 +5,19 @@ namespace TamagotchiMauiApp;
 public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
 	public event PropertyChangedEventHandler PropertyChanged;
-	private float Hunger { get; set; } = 100.0f;
+	public float Hunger { get; set; } = 0.0f;
 	public string HungerText => Hunger switch
 	{
-		0 => "Mijn buik is lekker rond! ",
-		< 25 => "O een kleine lekkere late night snack kan er wel in. ",
-		< 50 => "Ah Ooooh De lunch begint eraan te komen. ",
-		< 75 => "IK ZO'N ERGE HONGER!!!!! ",
-		< 85 => "OMG JE ZIET MIJN BOTTEN BIJNA ZO VEEL HONGER HEB IK!!!",
-		< 90 => "Ik sterf!",
-		< 100 => "Dag mijn Jongen",
+		0.0f => "Mijn buik is lekker rond! ",
+		<= 25.0f => "O een kleine lekkere late night snack kan er wel in. ",
+		<= 50.0f => "Ah Ooooh De lunch begint eraan te komen. ",
+		<= 75.0f => "IK ZO'N ERGE HONGER!!!!! ",
+		<= 85.0f => "OMG JE ZIET MIJN BOTTEN BIJNA ZO VEEL HONGER HEB IK!!!",
+		<= 90.0f => "Ik sterf!",
+		<= 100.0f => "Dag mijn Jongen",
+		_ => throw new ArgumentException("Impossible")
 	};
+	//public string HungerTextAmount => Hunger.ToString();
 
 	public MainPage()
 	{
@@ -30,15 +32,18 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 		Navigation.PushAsync(new FoodPage());
 	}
 
-	private void GeefEten(object sender, EventArgs e)
-	{
-		Hunger -= 1.0f;
-	}
-
 	private void ToSlurpHallFunction(object sender, EventArgs e)
 	{
 		SemanticScreenReader.Default.Announce("To Slurp Hall");
 		Navigation.PushAsync(new SlurpPage());
 	}
-}
 
+	private void GiveFood_Clicked(object sender, EventArgs e)
+	{
+		if (Hunger <= 100.0f)
+		{
+			Hunger += 1.0f;
+		}
+		HungerAmountText.Text = $"Clicked {Hunger} time";
+	}
+}
